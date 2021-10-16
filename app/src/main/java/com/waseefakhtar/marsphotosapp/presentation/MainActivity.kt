80b20c3ced38de0776971748093a03dd662f797e
Waseef Akhtar <waseef@degoo.com>
2021-10-16 20:40:48 +0500

    Add FragmentContainerView to MainActivity, add nav_graph

--- app/src/main/java/com/waseefakhtar/marsphotosapp/presentation/MainActivity.kt
+++ app/src/main/java/com/waseefakhtar/marsphotosapp/presentation/MainActivity.kt
@@ -3,12 +3,17 @@ package com.waseefakhtar.marsphotosapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.waseefakhtar.marsphotosapp.R
+import com.waseefakhtar.marsphotosapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
